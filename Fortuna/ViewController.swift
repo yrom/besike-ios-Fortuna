d43d756d9ac028442a99e237a6826ba22d61eedd
yrom <yrom@qq.com>
2014-10-17 23:51:51 +0800

    display a random quote

--- Fortuna/ViewController.swift
+++ Fortuna/ViewController.swift
@@ -18,8 +18,22 @@ class ViewController: UIViewController {
         println("view: \(self.view)")
         println("view's frame: \(self.view.frame)")
         println("viewDidLoad quotationTextView: \(quotationTextView)")
+        
+        displayRandomQuote()
     }
-
+    
+    func displayRandomQuote() {
+        let delegate = UIApplication.sharedApplication().delegate as AppDelegate
+        // quotes = radomly choose positiveQuotations or negativeQuotations
+        let quotes = random(2) == 1 ? delegate.positiveQuotes : delegate.negativeQuotes
+        // quote = randomly choose an element from quotes
+        let quote = quotes[random(quotes.count)]
+        quotationTextView.text = quote
+    }
+    // 0 <= r < count
+    func random(count: Int) -> Int{
+        return Int(arc4random_uniform(UInt32(count)))
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         
